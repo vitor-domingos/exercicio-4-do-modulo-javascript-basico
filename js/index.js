820a62653f7878fd8917893c8ea63237c35eb6fb
Vitor Domingos <vm486092@gmail.com>
2023-10-19 19:20:29 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+// Início de um loop 'for' que inicializa uma variável 'i' com 0.
+// O loop vai continuar enquanto 'i' for menor ou igual a 10.
+// A cada iteração, 'i' é incrementada em 1.
+
+for (let i = 0; i <= 10; i++) {
+    // Exibe uma mensagem no console que mostra o número atual 'i'.
+    console.log("O número atual é o" + i);
+}
+
+// O loop 'for' irá executar 11 vezes, começando de 0 e indo até 10,
+// e exibirá a mensagem no console a cada iteração, mostrando o valor atual de 'i'.
